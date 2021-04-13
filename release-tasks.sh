b01ad218471d21889d0956c06159c27132fbedc3
santius0 <santius1995@gmail.com>
2021-04-13 19:16:37 -0400

    updated release tasks

---
+++ release-tasks.sh
@@ -0,0 +1,12 @@
+python manage.py migrate
+
+python manage.py loaddata users
+python manage.py loaddata technologies
+python manage.py loaddata languages
+python manage.py loaddata clients
+python manage.py loaddata jobs
+python manage.py loaddata social_media
+python manage.py loaddata projects
+
+python manage.py loaddata profiles
+python manage.py loaddata about_me
